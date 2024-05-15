8e41c75bc3476d9393cf104c3389636f0373b3b0
Roman <166714007+shtemisu@users.noreply.github.com>
2024-05-15 12:19:08 +0900

    Update HisRusBot.py
    Added middleware

--- HisRusBot.py
+++ HisRusBot.py
@@ -3,13 +3,15 @@ from aiogram import Dispatcher, Bot
 from aiogram.fsm.storage.memory import MemoryStorage
 import logging
 from app.handlers import router1
+from Other import config
+from aiogram.utils.chat_action import ChatActionMiddleware
 
 
 async def main():
-    bot = Bot("ADD_Token", parse_mode="HTML")
+    bot = Bot(token=config.BOT_TOKEN, parse_mode="HTML")
     dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(router1)
-
+    dp.message.middleware(ChatActionMiddleware())
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
